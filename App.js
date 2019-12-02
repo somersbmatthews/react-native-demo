206fb3c20f30d30e706e812ccf447db10e730f4a
Somers Matthews <somersbmatthews@gmail.com>
2019-12-02 13:25:51 -0600

    fixed missing components

--- App.js
+++ App.js
@@ -1,9 +1,9 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
 
 export default function App() {
   return (
-    <View style={{}}> 
+    <View style={{padding: 10}}> 
       <View>
         <TextInput />
         <Button title="ADD" />
