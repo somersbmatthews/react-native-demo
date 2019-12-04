231a2dc12766e72fa3d59894f53d474259346df1
Somers Matthews <somersbmatthews@gmail.com>
2019-12-04 02:43:10 -0600

    added components directory

--- App.js
+++ App.js
@@ -3,9 +3,15 @@ import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
 
 export default function App() {
   return (
-    <View style={{padding: 10}}> 
-      <View>
-        <TextInput />
+    <View style={{padding: 50}}> 
+      <View 
+        style={{flexDirection: "row", 
+        justifyContent: "space-between",
+        alignItems: "center"}}>
+        <TextInput 
+          placeholder="Course Goal"
+          style={{width: "80%", borderColor: "black", borderWidth: 1, padding: 10 }}
+        />
         <Button title="ADD" />
       </View>
     </View>
